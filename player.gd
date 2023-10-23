2483df9a327e1942d6a0fedbd76644c2ca0f849b
Waffle6056 <114373955+Waffle6056@users.noreply.github.com>
2023-10-23 17:14:35 -0500

    jumping
    -space to jump
    -going down y layers is still super buggy

--- player.gd
+++ player.gd
@@ -3,18 +3,24 @@ extends CharacterBody2D
 var tilemaps = [0,0,0]
 var y = 0
 var currentmap
+var jumping = false
 
 const SPEED = 300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func movey(newy):
+func jump(offset):
+	jumping = true
 	set_collision_mask_value(y+1, false)
-	y = newy
+	y += offset
+	$Wiz/AnimationPlayer.play("jump")
+	$JumpTimer.start()
+	
+func _on_timer_timeout():
 	set_collision_mask_value(y+1, true)
 	print(y)
-	
+	jumping = false
 func _physics_process(delta):
 	# Add the gravity.
 	if (Input.is_action_pressed("Up")):
@@ -25,8 +31,10 @@ func _physics_process(delta):
 		velocity.x += SPEED
 	if (Input.is_action_pressed("Left")):
 		velocity.x += -SPEED
-	if (Input.is_action_just_pressed("testjump")):
-		movey(y+1)
+	if (Input.is_action_just_pressed("Jump")):
+		jump(1)
+	if ((velocity.x != 0 or velocity.y != 0) and not jumping):
+		$Wiz/AnimationPlayer.play("walking")
 	move_and_slide()
 	velocity = Vector2(0,0)
 func _ready():
@@ -40,7 +48,7 @@ func collcheck():
 		var cell = tilemaps[y].local_to_map(coll.get_position()/2)	
 		var tile_id = tilemaps[y].get_cell_atlas_coords(y,cell)
 		
-		if (tile_id.x == 4):
+		if (tile_id.x == 4 and not jumping):
 			set_collision_mask_value(y+1, false)
 			y -= 1
 			if (y <= -1):
@@ -48,11 +56,11 @@ func collcheck():
 			else:
 				set_collision_mask_value(y+1, true)
 				position -= coll.get_normal()*18
-				print(coll.get_normal())
+				print(y)
 				
 			
 			
 		
 func _process(delta):
 	collcheck()
-
+ # Replace with function body.
